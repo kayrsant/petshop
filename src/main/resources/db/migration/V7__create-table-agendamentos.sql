dc9966c3e6ad186daed7d505cb85f818a5271811
kayrsant <kayrsant@gmail.com>
2023-11-15 16:58:05 -0300

    update code

--- src/main/resources/db/migration/V7__create-table-agendamentos.sql
+++ src/main/resources/db/migration/V7__create-table-agendamentos.sql
@@ -5,15 +5,11 @@ CREATE TABLE agendamentos (
                               id_pet INT NOT NULL REFERENCES pets(id),
                               id_funcionario INT NOT NULL REFERENCES funcionarios(id),
                               data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                              valor DECIMAL(10, 2) NOT NULL DEFAULT 0
-);
-
--- Tabela para os serviços realizados durante o atendimento
-CREATE TABLE servicos_agendamento (
-                                      id SERIAL NOT NULL PRIMARY KEY,
-                                      id_agendamento INT NOT NULL REFERENCES agendamentos(id),
-                                      id_servico INT NOT NULL REFERENCES servicos(id),
-                                      valor DECIMAL(10, 2) NOT NULL DEFAULT 0
+                              valor DECIMAL(10, 2) NOT NULL DEFAULT 0,
+                              id_servico INT NOT NULL REFERENCES servicos(id),
+                              status VARCHAR(10) NOT NULL DEFAULT 'aberto',
+                              data_inicio TIMESTAMP NOT NULL,
+                              data_fim TIMESTAMP NOT NULL
 );
 
 -- Tabela para os produtos usados no atendimento
@@ -21,6 +17,6 @@ CREATE TABLE produtos_agendamento (
                                       id SERIAL NOT NULL PRIMARY KEY,
                                       id_agendamento INT NOT NULL REFERENCES agendamentos(id),
                                       id_produto INT NOT NULL REFERENCES produtos(id),
-                                      quantidade INT NOT NULL DEFAULT 1
+                                      quantidade INT NOT NULL DEFAULT 1,
+                                      valor DECIMAL(10, 2) NOT NULL DEFAULT 0
 );
-
